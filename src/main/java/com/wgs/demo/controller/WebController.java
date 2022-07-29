888304a31c33d4a13a53faef5d705f9c9cff045e
divyanshdubey <divyanshduby9100@gmail.com>
2022-07-29 18:50:47 +0530

    06:50 29-07-22

--- src/main/java/com/wgs/demo/controller/WebController.java
+++ src/main/java/com/wgs/demo/controller/WebController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.wgs.demo.cust.Customer;
@@ -161,5 +162,12 @@ public class WebController {
 	@RequestMapping("customer")
 	public String adminUi() {
 		return "views/customer";
+	}
+	@RequestMapping("editAccountDetails")
+	public String editAccountDetails(Customer customer,Model model) {
+		String mes = "Work in Prpgress";
+		System.out.println(mes);
+		model.addAttribute("cust", mes);
+		return "views/customerAccountDetails";
 	}
 }
