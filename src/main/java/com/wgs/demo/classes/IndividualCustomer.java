0f881839a2755587c553af2a210f3be42f58c7f5
Divyanshu Dubey <divyanshdubey9100@gmail.com>
2022-09-17 06:44:51 +0530

    newimpro

--- src/main/java/com/wgs/demo/classes/IndividualCustomer.java
+++ src/main/java/com/wgs/demo/classes/IndividualCustomer.java
@@ -6,16 +6,9 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.springframework.stereotype.Component;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 @Table(name = "IndividualCustomer", uniqueConstraints = @UniqueConstraint(columnNames = { "trxId" }))
 @Entity
 @Component
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class IndividualCustomer {
 	@Id
 	private int trxId;
@@ -26,6 +19,78 @@ public class IndividualCustomer {
 	private int amtBefTrx;
 	private int trxAmt;
 	private int currentBalance;
+	public IndividualCustomer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public IndividualCustomer(int trxId, int accNo, String custName, String trxMode, String trxDate, int amtBefTrx,
+			int trxAmt, int currentBalance) {
+		super();
+		this.trxId = trxId;
+		this.accNo = accNo;
+		this.custName = custName;
+		this.trxMode = trxMode;
+		this.trxDate = trxDate;
+		this.amtBefTrx = amtBefTrx;
+		this.trxAmt = trxAmt;
+		this.currentBalance = currentBalance;
+	}
+	public int getTrxId() {
+		return trxId;
+	}
+	public void setTrxId(int trxId) {
+		this.trxId = trxId;
+	}
+	public int getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getTrxMode() {
+		return trxMode;
+	}
+	public void setTrxMode(String trxMode) {
+		this.trxMode = trxMode;
+	}
+	public String getTrxDate() {
+		return trxDate;
+	}
+	public void setTrxDate(String trxDate) {
+		this.trxDate = trxDate;
+	}
+	public int getAmtBefTrx() {
+		return amtBefTrx;
+	}
+	public void setAmtBefTrx(int amtBefTrx) {
+		this.amtBefTrx = amtBefTrx;
+	}
+	public int getTrxAmt() {
+		return trxAmt;
+	}
+	public void setTrxAmt(int trxAmt) {
+		this.trxAmt = trxAmt;
+	}
+	public int getCurrentBalance() {
+		return currentBalance;
+	}
+	public void setCurrentBalance(int currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+	@Override
+	public String toString() {
+		return "IndividualCustomer [trxId=" + trxId + ", accNo=" + accNo + ", custName=" + custName + ", trxMode="
+				+ trxMode + ", trxDate=" + trxDate + ", amtBefTrx=" + amtBefTrx + ", trxAmt=" + trxAmt
+				+ ", currentBalance=" + currentBalance + "]";
+	}
+	
+	
 	
 	
 }
--- src/main/java/com/wgs/demo/classes/Owner.java
+++ src/main/java/com/wgs/demo/classes/Owner.java
@@ -7,16 +7,10 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import org.springframework.stereotype.Component;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Table(name = "owner", uniqueConstraints = @UniqueConstraint(columnNames = { "userId", "mobile" }))
 @Entity
 @Component
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Owner {
 	@Id
 	@GeneratedValue
@@ -26,4 +20,60 @@ public class Owner {
 	private String mobile;
 	private String gender;
 	private String pass;
+	public Owner() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Owner(int id, String userId, String name, String mobile, String gender, String pass) {
+		super();
+		this.id = id;
+		this.userId = userId;
+		this.name = name;
+		this.mobile = mobile;
+		this.gender = gender;
+		this.pass = pass;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	@Override
+	public String toString() {
+		return "Owner [id=" + id + ", userId=" + userId + ", name=" + name + ", mobile=" + mobile + ", gender=" + gender
+				+ ", pass=" + pass + "]";
+	}
+	
+	
 }
